dab38fd56a1dfb153be7946da69da4107d6e47f8
morbos <hedleyrainnie@gmail.com>
2019-12-23 23:32:38 -0800

    - Cleanup L522 glue code.

--- L/L522/test/glue/veneer_s.c
+++ L/L522/test/glue/veneer_s.c
@@ -18,43 +18,10 @@ void s_to_ns(void)
     REG32(0xe000ed8c) = 0xcff; // W/o this.... you get a usage fault..(!)
 
     REG32(0xe002ed08) = NON_SECURE_START; // Set the VECTORS
-    /* Set non-secure vector table */
-//    SCB_NS->VTOR = NON_SECURE_START;
-
-    is_secure(0x20000000);
-    is_secure(0x20010000);
-    is_secure(0x30000000);
-    is_secure(0x30010000);
-    is_secure(0x30020000);
-    is_secure(NON_SECURE_START);
 
     /* Get non-secure reset handler */
     ResetHandler_ns = (funcptr_ns)(*((u32 *)((NON_SECURE_START) + 4U)));
 
     ResetHandler_ns();
 
-}
-void init_idau(void)
-{
-    int i;
-    u32 is_secure(u32);
-    for(i=0;i < 8;i++) {
-	REG32(0x50003700 + (i * 4)) = 0x17;
-    }
-    for(;i < 16;i++) {
-	REG32(0x50003700 + (i * 4)) = 0x17;
-    }
-    for(;i < 24;i++) {
-	REG32(0x50003700 + (i * 4)) = 0x7;
-    }
-
-    // Now need to add the NS veneer
-    REG32(0x50003540) = 15; // last 8k region of 0x2001e000
-    REG32(0x50003544) = 8; // A 4k region
-    REG32(0x50003548) = 15; // last 8k region of 0x2001e000
-    REG32(0x5000354c) = 8; // A 4k region
-
-    is_secure(0x2001e000);
-
-    REG32(0xe000edd0) = 2; // <<<<< Magic to mark all RAM as NS (bit1 = 1)
 }
